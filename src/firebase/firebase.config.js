1ff13ab28cf8b2045e00b32c1e6bd3809f643be0
alsaif123 <alsaif200200@gmail.com>
2021-10-30 13:27:20 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyBjFpmmfcwVOikJ1rjoBLmf_l-oCa-Vr_4",
+  authDomain: "tourisom-authentication.firebaseapp.com",
+  projectId: "tourisom-authentication",
+  storageBucket: "tourisom-authentication.appspot.com",
+  messagingSenderId: "663044555491",
+  appId: "1:663044555491:web:7b07cc5f5044211d0cbbd6"
+};
+export default firebaseConfig;
